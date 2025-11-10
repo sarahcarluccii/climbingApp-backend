214ef966bafab3f313fdd8236a946275532f2b5f
sarahcarluccii <sarahlucci25@gmail.com>
2025-11-10 15:19:56 -0500

    initial backend commit

---
+++ src/main/java/org/climbing/app/ClimbingAppApplication.java
@@ -0,0 +1,13 @@
+package org.climbing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/climbing/app/ClimbingApp/ClimbingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.climbing.app.ClimbingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
